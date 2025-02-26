10a2b233aceacafb245c60e71c34ecf428f0abb0
Ryan <ryansoaresjorvino@gmail.com>
2025-02-26 19:35:01 -0300

    Coments class location

--- _class/location.py
+++ _class/location.py
@@ -7,19 +7,24 @@ class Location():
    
    @property      
    def x(self) -> int:
+      """Returns the x-coordinate of the location."""
       return self.__x
       
    @x.setter
    def x(self, x: int):
+      """Sets the x-coordinate of the location."""
       self.__x = x
       
    @property      
    def y(self) -> int:
+      """Returns the y-coordinate of the location."""
       return self.__y
       
    @y.setter
    def y(self, y: int):
+      """Sets the y-coordinate of the location."""
       self.__y = y
       
    def __repr__(self) -> str:
+      """Returns a string representation of the Location object."""
       return f"{type(self).__name__}({self.x!r}, {self.y!r})"
